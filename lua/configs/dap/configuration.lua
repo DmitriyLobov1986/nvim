030a257182155745f59167926fbf2e058d8f0944
dmitriy.lobov <dmitriy.Lobov@intercontaudit.ru>
2025-02-20 17:27:58 +0300

    dap and dapui configuration

--- lua/configs/dap/configuration.lua
+++ lua/configs/dap/configuration.lua
@@ -5,29 +5,12 @@ require('dap').adapters['pwa-node'] = {
   host = '::1',
   port = '${port}',
   executable = {
-    -- command = "js-debug-adapter",
-    command = 'node',
+    command = 'js-debug-adapter',
+    -- command = 'node',
     args = {
       -- '/home/user001/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js',
-      '/root/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js',
+      -- '/root/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js',
       '${port}',
     },
   },
-}
-
-require('dap').configurations.javascript = {
-  {
-    type = 'pwa-node',
-    request = 'launch',
-    name = 'FileDebug',
-    program = '${file}',
-    cwd = '${workspaceFolder}',
-  },
-  -- {
-  --   type = 'pwa-node',
-  --   request = 'launch',
-  --   name = 'FileDebug42',
-  --   program = '${file}',
-  --   cwd = '${workspaceFolder}',
-  -- },
 }
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -6,4 +6,9 @@ return {
       require 'dapui'
     end,
   },
+  {
+    'rcarriga/nvim-dap-ui',
+    opts = require 'configs.dap.dapui_config',
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
+  },
 }
