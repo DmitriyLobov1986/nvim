ff6d40b3da637b49d00a9a89b8d04e131e9db986
dmitriy.Lobov <dmitriy.Lobov@intercontaudit.ru>
2025-12-18 11:15:24 +0300

    update treesiter config

---
+++ lua/configs/treesitter/init.lua
@@ -0,0 +1,50 @@
+local opts = {
+  ensure_installed = {
+    'vim',
+    'lua',
+    'vimdoc',
+    'html',
+    'css',
+    'javascript',
+    'typescript',
+    'json',
+    'jsonc',
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = {
+          query = '@class.inner',
+          desc = 'Select inner part of a class region',
+        },
+        ['as'] = {
+          query = '@local.scope',
+          query_group = 'locals',
+          desc = 'Select language scope',
+        },
+      },
+      selection_modes = {
+        ['@parameter.outer'] = 'v', -- charwise
+        ['@function.outer'] = 'V', -- linewise
+        ['@class.outer'] = '<c-v>', -- blockwise
+      },
+      include_surrounding_whitespace = true,
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
+  },
+}
+
+return opts
---
+++ lua/plugins/treesitter-textobjects.lua
@@ -0,0 +1,7 @@
+return {
+  lazy = false,
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  },
+}
